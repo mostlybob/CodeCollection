e5844e561b5e8342ab169abe51c69c932ba12340
Bob Campbell <mostlybob@gmail.com>
2023-04-02 20:44:20 -0500

    added demo for expressions

--- Funcs/Funcs.cs
+++ Funcs/Funcs.cs
@@ -5,11 +5,13 @@ public class Demo : IDemo
     public void Run()
     {
         SimpleInstanceDemo();
+
+        ExpressionsDemo();
     }
 
     private static void SimpleInstanceDemo()
     {
-        Console.WriteLine("Running SimpleInstanceDemo...");
+        OutputMessage("Running SimpleInstanceDemo...");
         OutputMessage("instantiating Foo now and calling Bar");
         IFoo foo = new Foo();
         foo.Bar();
@@ -30,6 +32,34 @@ public class Demo : IDemo
         fooExpression().Bar();
 
         OutputMessage("Voilà!");
+    }
+
+    private static void ExpressionsDemo()
+    {
+        OutputMessage("Running ExpressionsDemo...");
+        OutputMessage("new up the expressions demos");
+
+        var demoNone = GetNewExpressionsDemoExample();
+        var demoFoo = GetNewExpressionsDemoExample();
+        var demoBar = GetNewExpressionsDemoExample();
+        var demoBoth = GetNewExpressionsDemoExample();
+
+        OutputMessage("call that doesn't call any of the expressions' methods");
+        demoNone.CallNothing();
+
+        OutputMessage("call that only calls Foo's method");
+        demoFoo.CallFoo();
+
+        OutputMessage("call that only calls Bar's method");
+        demoBar.CallBar();
+
+        OutputMessage("call that only calls both Foo's and Bar's methods");
+        demoBoth.CallFooAndBar();
+    }
+
+    private static DemoWithExpressions GetNewExpressionsDemoExample()
+    {
+        return new DemoWithExpressions(() => new Foo(), () => new Bar());
     }
 
     public static void OutputMessage(string message)
@@ -87,5 +117,75 @@ public class Bar : IBar
     public void Baz()
     {
         Demo.OutputMessage("\tBar.Baz() method code");
+    }
+}
+
+public class DemoWithExpressions
+{
+    private Func<IFoo> foo;
+    private Func<IBar> bar;
+    public DemoWithExpressions(Func<IFoo> foo, Func<IBar> bar)
+    {
+        this.foo = foo;
+        this.bar = bar;
+    }
+
+    public void CallNothing()
+    {
+        Demo.OutputMessage("\tneither foo nor bar had their methods called");
+
+        /*
+        Using expresssions for the injections means that no constructors
+        get called in this method. If this is the only method that gets
+        called during the lifetime of this object, the expressions for
+        foo & bar instance variables never get evaluated and the respective
+        constructors never get called, since they're never needed. As a 
+        result, that overhead is never incurred, which makes for better
+        resource allocation.
+        */
+    }
+
+    public void CallFoo()
+    {
+        Demo.OutputMessage("\tcalling foo now");
+        foo().Bar();
+
+        /*
+        When the foo expression is evaluated, the constructor gets invoked,
+        and only then. Despite the fact that IBar has been injected in this
+        class, its constructor has not been invoked yet. If this is the only 
+        method called during this object's lifespan, or if other methods 
+        in this class only call methods on foo, the IBar instance expression 
+        variable will never be invoked and the app will not incur that overhead.
+        */
+    }
+
+    public void CallBar()
+    {
+        Demo.OutputMessage("\tcalling bar now");
+        bar().Baz();
+
+        /*
+        Similar to the previous method, if this is the only method called,
+        foo's expression will not be evaluated and the app will not incur
+        that overhead
+        */
+    }
+
+    public void CallFooAndBar()
+    {
+        Demo.OutputMessage("\tcalling foo.Bar now");
+        foo().Bar();
+
+        Demo.Sleep5s();
+
+        Demo.OutputMessage("\tcalling bar.Baz now");
+        bar().Baz();
+
+        /*
+        This method evaluates both expressions and executes their methods; however
+        even with both expressions being invoked, their respective constructors
+        are still only called immediately before the method invocation happens.
+        */
     }
 }
